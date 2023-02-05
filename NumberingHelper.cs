310a289a76f420aa5ae2a12fbe8fee7b8c8af8bb
Laftek <balintmat@gmail.com>
2023-02-04 18:29:36 -0800

    updater -> test no.4

--- NumberingHelper.cs
+++ NumberingHelper.cs
@@ -6,13 +6,5 @@
         public int MaxLimit { get; set; }
         public IEnumerable<int>? LottieAnimList { get; set; }
         public bool isUpdateReady { get; set; } = false;
-
-
-        public void PWAUpdaterService_NextVersionIsWaiting(object sender, EventArgs e)
-        {
-            // Add your logic here to show the notification to the users
-            isUpdateReady = true;
-            
-        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 using BlazorAppWasmPWALottieRive;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+using Toolbelt.Blazor.PWA.Updater.Service;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<NumberingHelper>();
-
+builder.Services.AddSingleton<IPWAUpdaterService>();
+builder.Services.AddPWAUpdater();
 await builder.Build().RunAsync();
 //Copyright © 2021 Design Barn Inc.
 //Permission is hereby granted, free of charge, to any person obtaining a copy of the public animation files available for download at the LottieFiles site(“Files”) to download, reproduce, modify, publish, distribute, publicly display, and publicly digitally perform such Files, including for commercial purposes, provided that any display, publication, performance, or distribution of Files must contain(and be subject to) the same terms and conditions of this license.Modifications to Files are deemed derivative works and must also be expressly distributed under the same terms and conditions of this license.You may not purport to impose any additional or different terms or conditions on, or apply any technical measures that restrict exercise of, the rights granted under this license.This license does not include the right to collect or compile Files from LottieFiles to replicate or develop a similar or competing service.
