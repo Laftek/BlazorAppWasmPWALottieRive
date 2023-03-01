cd9af3eb2a3d5d0ea8f197a020127d2631297eba
Laftek <balintmat@gmail.com>
2023-03-01 15:21:44 +0100

    Version V2.1 - fixing UI (cleanup + centering items); implementing 'go back' button; implementing non elastic scroll for animation pages (only allowed on index page); code cleanup

---
+++ wwwroot/scripts/disableElasticScroll.js
@@ -0,0 +1,13 @@
+﻿let touchMoveListener = null;
+export function disableElasticScroll() {
+    touchMoveListener = function (event) {
+        event.preventDefault();
+    };
+    document.addEventListener('touchmove', touchMoveListener, { passive: false });
+}
+export function enableElasticScroll() {
+    if (touchMoveListener) {
+        document.removeEventListener('touchmove', touchMoveListener);
+        touchMoveListener = null;
+    }
+}
